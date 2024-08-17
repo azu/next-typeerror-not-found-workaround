10f57454ae69acac4014f61078d72e0871e86b79
azu <azuciao@gmail.com>
2024-08-17 17:46:14 +0900

    fix: add workaround example

--- next.config.js
+++ next.config.js
@@ -1,7 +1,16 @@
 /** @type {import("next").NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  pageExtensions: ["page.tsx", "page.ts"],
+    reactStrictMode: true,
+    pageExtensions: [
+        "page.tsx",
+        "page.ts",
+        // FIXME: Next.js has a bug which does not resolve not-found.page.tsx corretly
+        // Instead, use `not-found.ts` as a workaround
+        // "ts" is required to resolve `not-found.ts`
+        // https://github.com/vercel/next.js/issues/65447
+        "ts"
+    
+    ],
 };
 
 module.exports = nextConfig;
